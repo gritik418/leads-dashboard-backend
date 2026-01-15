1cb1b14d6960dd09f163c709bae20de47df7a6d8
Ritik <160255700+gritik418@users.noreply.github.com>
2026-01-15 15:20:37 +0530

    feat: add logout route

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -124,3 +124,18 @@ export const userLogin = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const logout = async (req: Request, res: Response) => {
+  try {
+    res.clearCookie(AUTH_TOKEN, cookieOptions);
+    return res.status(200).json({
+      success: true,
+      message: "Logged out successfully!",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { userLogin, userRegister } from "../controllers/auth.controller.js";
+import {
+  logout,
+  userLogin,
+  userRegister,
+} from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.post("/register", userRegister);
 
 router.post("/login", userLogin);
+
+router.post("/logout", logout);
 
 export default router;
